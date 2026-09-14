274a770ca8b0a83b5321b5b9b50ce20bac8566dd
kurtmckenzie <kmcke002@gmail.com>
2026-09-14 13:52:54 -0500

    Update Supabase publishable key in config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // NEVER use a service_role/secret key in this file.
 window.SUPABASE_CONFIG = {
   url: "https://nbyalyokwmtwwblfbvei.supabase.co",
-  key: "YOUR_PUBLISHABLE_KEY"
+  key: "sb_publishable_vw0l7M_lsdbjYLbTAeq-YA_D4X3LpqP"
 };
