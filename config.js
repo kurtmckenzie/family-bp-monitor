45b3a3d8da0f5e0345b61f6d8181d9a22c48b8af
kurtmckenzie <kmcke002@gmail.com>
2026-09-14 16:52:09 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Paste your Supabase Project URL and browser-safe publishable/anon key here.
-// NEVER use a service_role/secret key in this file.
-window.SUPABASE_CONFIG = {
-  url: "https://nbyalyokwmtwwblfbvei.supabase.co",
-  key: "sb_publishable_vw0l7M_lsdbjYLbTAeq-YA_D4X3LpqP"
-};
